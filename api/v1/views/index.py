0235e6d5d163ee5b6746dd80d72ca164a360d967
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-04-19 23:19:04 +0000

    count objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,30 @@
 """Index file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
 
 @app_views.route("/status")
 def status():
+    """Displays status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def obj_stats():
+    """Get the number of each object"""
+    obj = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "user": storage.count(User),
+            "place": storage.count(Place)
+            }
+    return jsonify(obj)
